91358de4dc196e07be1b541b4767e863d5192880
kusui-user <kusui@foodnetwork.co.jp>
2021-06-20 10:53:23 +0900

    更新

--- components/FormItemButton.vue
+++ components/FormItemButton.vue
@@ -1,10 +1,10 @@
 <template>
   <button
     v-bind="$attrs"
-    class="button form-item-button txt-center font-500 font-futura"
+    class="button form-item-button "
     @click="$emit('click')"
   >
-    <slot>LABEL</slot>
+    <slot></slot>
   </button>
 </template>
 
@@ -20,22 +20,14 @@ export default {}
   line-height: 1.4em;
   letter-spacing: 0.12em;
   justify-content: center;
-  color: var(--color-font-white);
-  background-color: var(--color-bg-black);
+  text-align: center;
+  color: white;
+  background-color:black;
   border: none;
   transition: all 0.024s linear;
 
   &:hover {
-    background-color: var(--color-bg-black-64);
-  }
-
-  &.button-light {
-    color: var(--color-font-default);
-    background-color: var(--color-bg-default);
-
-    &:hover {
-      background-color: var(--color-bg-white-64);
-    }
+    background-color: rgb(39, 38, 38);
   }
 
   &[disabled] {
